92e124d9d4ebf41ba28ffe63b335f22c7a649618
X-Liang <1129584401@qq.com>
2019-07-29 23:22:52 +0800

    动态规划746

--- LeetCode-Java/src/Tree/LeetCode/TreeNode.java
+++ LeetCode-Java/src/Tree/LeetCode/TreeNode.java
@@ -136,6 +136,29 @@ public class TreeNode {
         }
         return pathSum(root, 0, sum);
     }
+
+    /**
+     * 根据一棵树的中序遍历与后序遍历构造二叉树。
+     *
+     * 注意:
+     * 你可以假设树中没有重复的元素。
+     *
+     * 例如，给出
+     *
+     * 中序遍历 inorder = [9,3,15,20,7]
+     * 后序遍历 postorder = [9,15,7,20,3]
+     * 返回如下的二叉树：
+     *
+     *     3
+     *    / \
+     *   9  20
+     *     /  \
+     *    15   7
+     */
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        return null;
+    }
+
     private boolean pathSum(TreeNode node, int currentValue, int sum) {
         if (node == null) {
             return false;
