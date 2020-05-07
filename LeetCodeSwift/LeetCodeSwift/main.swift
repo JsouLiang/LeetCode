cd52e61ae841b9717b126e3ecbdd13739ffa5d00
X-Liang <1129584401@qq.com>
2020-05-07 19:46:02 +0800

    update: Swift project Rename

---
+++ LeetCodeSwift/LeetCodeSwift/main.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  LeetCodeSwift
+//
+//  Created by Liang on 2020/5/7.
+//  Copyright © 2020 imieutan. All rights reserved.
+//
+
+import Foundation
+
---
+++ LeetCodeSwift/LeetCodeSwiftTests/Base Data Struct/Link/LeetCode21Tests.swift
@@ -0,0 +1,18 @@
+//
+//  LeetCode21Tests.swift
+//  LeetCodeSwiftTests
+//
+//  Created by Liang on 2020/5/7.
+//  Copyright © 2020 imieutan. All rights reserved.
+//
+
+import XCTest
+@testable import LeetCodeSwift
+
+class LeetCode21Tests: XCTestCase {
+
+    func testExample() throws {
+        let leetcode = LeetCode21()
+    }
+
+}
